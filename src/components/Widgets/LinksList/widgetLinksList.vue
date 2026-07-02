2c5acd6e8d29b2ce36a08bd2ce36ff52922d3451
p.kliukin <p.kliukin@gaijin.team>
2026-07-02 19:38:26 +0300

    Design for the links list block + some articles for mens calendar page

--- src/components/Widgets/LinksList/widgetLinksList.vue
+++ src/components/Widgets/LinksList/widgetLinksList.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import styles from './widgetLinksList.module.scss';
 import IconLines from '@/assets/img/svg/icon-lines.svg';
+
+const modules = import.meta.glob('@/assets/img/animals/block_top/*', { eager: true, query: '?url', import: 'default' });
+const images = Object.values(modules) as string[];
+const artImage = images[Math.floor(Math.random() * images.length)];
 </script>
 
 <template>
   <div :class="styles.widgetLinksList">
+    <img :class="styles.widgetLinksList__art" :src="artImage" alt="" />
+
     <div :class="styles.widgetLinksList__title">
       <IconLines />
 

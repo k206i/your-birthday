1145d051d13517a17a705ab1cc7a2d60dc640f62
p.kliukin <pavel.klyukin@gmail.com>
2026-08-08 02:01:40 +0300

    Donation link + life weeks small refactoring

--- src/components/AppFooter/appFooter.vue
+++ src/components/AppFooter/appFooter.vue
@@ -6,7 +6,13 @@ import {IonFooter, IonTitle, IonToolbar} from '@ionic/vue';
 <template>
   <ion-footer :class="styles.appFooter" :translucent="true">
     <ion-toolbar>
-      <a href="https://pay.cloudtips.ru/p/460b7ab7" target="_blank">Чаевые</a>
+      💜
+      <a :class="styles.appFooter__link"
+         href="https://pay.cloudtips.ru/p/460b7ab7"
+         target="_blank"
+      >
+        Приложение держится на этих людях — и на вас
+      </a>
     </ion-toolbar>
   </ion-footer>
 </template>
